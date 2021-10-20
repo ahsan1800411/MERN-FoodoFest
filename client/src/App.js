8c86c8b5ac61847cb5f60c63eaf43ddab5455ee0
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-10-20 07:34:29 +0500

    Basic react app setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Mern Pizza</h1>
+    </div>
+  );
+}
+
+export default App;
