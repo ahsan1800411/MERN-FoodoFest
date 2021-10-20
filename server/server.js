b034e05fb9c7e903accf165d11eb067e8ab5cee8
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-10-20 10:38:39 +0500

    Connected with the DB

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDatabase = require('./config/db');
 
 dotenv.config({ path: 'server/.env' });
 
 const port = process.env.PORT;
+
+connectDatabase();
 
 app.listen(5000, () => {
   console.log(`Server is up and running on  port ${port}`);
